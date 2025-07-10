0396583700c164fc4d55c4c5c2b54302a1af9b4a
AlzyWelzy <welzyalzy@gmail.com>
2025-07-10 18:08:01 +0530

    .

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from app.api.v1 import users
 
 app = FastAPI()
 
-app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
+# app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
+app.include_router(users.router, prefix="/v1/users", tags=["Users"])
+
 
 @app.get("/")
 def root():
