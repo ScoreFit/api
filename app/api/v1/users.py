4ea45bb746435d9b7f22ebc6bcb32296233e1188
AlzyWelzy <welzyalzy@gmail.com>
2025-07-10 17:55:57 +0530

    .

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from app.core.redis import test_connection
 
 router = APIRouter()
 
@@ -6,9 +7,6 @@ router = APIRouter()
 @router.get("/")
 def get_users():
     return [{"username": "john"}, {"username": "doe"}]
-
-
-from app.core.redis import test_connection
 
 
 @router.get("/redis-test")
